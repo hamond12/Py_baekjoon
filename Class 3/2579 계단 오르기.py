9e2396fb43d0af4da7a971534edd7a95960ddda4
Juyoung Song <80014509+sjy1410@users.noreply.github.com>
2022-12-29 10:45:15 +0900

    계단오르기 게임(dp)

--- Class 3/2579 계단 오르기.py
+++ Class 3/2579 계단 오르기.py
@@ -1,18 +1,18 @@
-n = int(input())
+n = int(input()) #게단 수
 
-s = [int(input()) for i in range(n)]  # 계단높이 받을 배열
-dp = [0]*(n)  #n번째 계단을 밟았을 때의 점수 합
+s = [int(input()) for _ in range(n)] #계단 높이들
+dp = [0]*(n) #n번째 계단을 밟았을 때 점수 합
 
-# 계단이 2개 이하면 값 다 더해서 출력
-if len(s) <= 2:
+#계단이 2개이하면 점화식 필요 x
+if len(s)<=2:
     print(sum(s))
 
-else:
-    dp[0] = s[0]  # 첫번째 계단
-    dp[1] = s[0]+s[1]  # 두번째 계단
-    for i in range(2, n):  # 세번째 계단 부터 점화식 이용
-        dp[i] = max(dp[i-3]+s[i-1]+s[i], dp[i-2]+s[i])
-        # 연속해서 세 계단을 오를 순 없다
-        #dp[3]=max(s[0]+s[1]+s[3], s[1]+s[3])
+dp[0] = s[0] 
+dp[1] = s[0]+s[1]
+for i in range(2,n):
+    #연속된 세 개 계단 x, 마지막 도착 계단 밟아야 함.
+    #dp[3] = max(dp[0]+s[1]+s[3], dp[1]+s[3]) 계단이 4개라면
+    #i=3을 대입해 점화식 도출
+    dp[i] = max(dp[i-3]+s[i-2]+s[i], dp[i-2]+s[i])
 
-print(dp[n-1])  # n번째 계단을 밟았을 때의 합
+print(dp[n-1]) #배열은 0번지부터
